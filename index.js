c58fc28611109a8ed44507d6f7a7dd943a6e9c6b
Refia Candan Ozkalay <refia.candan@gmail.com>
2020-04-10 19:38:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let coolText = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function() {
+    coolText.textContent = "This is really cool!";
+  });
+   
